31e70503b5e7adfbf7ddb279091a49122bf9190c
taihim <tai.him18@gmail.com>
2025-02-04 00:14:34 +0100

    fix: fixed backprop bug by accumulating gradients

--- scripts/scratchpad.py
+++ scripts/scratchpad.py
@@ -19,23 +19,32 @@ from src import Value, visualize_graph
 
 
 # simple 2-input neuron visualization
-x1 = Value(2.0, label="x1") 
-x2 = Value(0, label="x2")
+# x1 = Value(2.0, label="x1") 
+# x2 = Value(0, label="x2")
 
-# weights for inputs
-w1 = Value(-3.0, label="w1")
-w2 = Value(1.0, label="w2")
+# # weights for inputs
+# w1 = Value(-3.0, label="w1")
+# w2 = Value(1.0, label="w2")
 
-# bias 
-b = Value(6.8813735870195432, label="b")
+# # bias 
+# b = Value(6.8813735870195432, label="b")
 
-x1w1 = x1 * w1; x1w1.label = "x1*w1"
-x2w2 = x2 * w2; x2w2.label = "x2*w2"
+# x1w1 = x1 * w1; x1w1.label = "x1*w1"
+# x2w2 = x2 * w2; x2w2.label = "x2*w2"
 
-x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = "x1*w1 + x2*w2"
-n = x1w1x2w2 + b; n.label="n"
-o = n.tanh(); o.label="o"
+# x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = "x1*w1 + x2*w2"
+# n = x1w1x2w2 + b; n.label="n"
+# o = n.tanh(); o.label="o"
 
-o.backward()
+# o.backward()
 
-visualize_graph(o, filename="neuron")
+# visualize_graph(o, filename="neuron")
+
+
+# repeated value backprop. to check for a bug. add these cases as unit tests
+a = Value(3.0, label="a") # grad should be 2 because db/da of a + a = 1 + 1 => 2
+b = a + a; b.label = "b"
+b.backward()
+visualize_graph(b, filename="repeated")
+
+# repeated value
--- src/value.py
+++ src/value.py
@@ -26,8 +26,9 @@ class Value:
 
         def _backward() -> None:
             # chain rule
-            self.grad = 1 * result.grad
-            other_value.grad = 1 * result.grad
+            # we use += to accumulate the gradients for when a Value is used multiple times in the expression graph
+            self.grad += 1 * result.grad
+            other_value.grad += 1 * result.grad
 
         self._backward = _backward
 
@@ -40,8 +41,8 @@ class Value:
 
         def _backward() -> None:
             # chain rule
-            self.grad = other_value.data * result.grad
-            other_value.grad = self.data * result.grad
+            self.grad += other_value.data * result.grad
+            other_value.grad += self.data * result.grad
 
         self._backward = _backward
 
@@ -55,7 +56,9 @@ class Value:
 
         def _backward() -> None:
             # chain rule
-            self.grad = (1 - t**2) * out.grad  # derivative of tanh(x) is 1-tanh(x)^2 and we already have tanh(x) i.e. t
+            self.grad += (
+                1 - t**2
+            ) * out.grad  # derivative of tanh(x) is 1-tanh(x)^2 and we already have tanh(x) i.e. t
 
         self._backward = _backward
 
@@ -64,7 +67,6 @@ class Value:
     def backward(self) -> None:
         """Perform a backward pass through the Value object and all its children."""
         self.grad = 1.0
-        self._backward()  # type: ignore[no-untyped-call]
         stack = [self]
         while stack:
             node = stack.pop()
