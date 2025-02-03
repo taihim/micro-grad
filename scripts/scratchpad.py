0e27fcc8a8697d987fccb9affb061f181e187d1a
taihim <tai.him18@gmail.com>
2025-02-03 23:59:41 +0100

    feat: added neuron viz to scratchpad

--- scripts/scratchpad.py
+++ scripts/scratchpad.py
@@ -2,33 +2,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 from src import Value, visualize_graph
 
-# def f(x):
-#     return 3 * x**2 - 4 * x + 5
 
 
-# xs = np.arange(-5, 5, 0.25)
-# ys = f(xs)
+# a = Value(2.0, label="a")
+# b = Value(-3.0, label="b")
+# c = Value(10.0, label="c")
+# e = a * b; e.label = "e"
+# d = e + c; d.label = "d"
+# f = Value(-2, label="f")
+# l = d * f; l.label = "l"
+# M = a * l; M.label = "M"
 
-# plt.plot(xs, ys)
-# plt.show()
+# M.backward()
 
-
-
-h = 0.01
-
-a = Value(2.0, label="a")
-b = Value(-3.0, label="b")
-c = Value(10.0, label="c")
-e = a * b; e.label = "e"
-d = e + c; d.label = "d"
-f = Value(-2, label="f")
-L = d * f; L.label = "L"
-# L.grad = 1.0
-
-L.backward()
+# visualize_graph(M, filename="viz4")
 
 
+# simple 2-input neuron visualization
+x1 = Value(2.0, label="x1") 
+x2 = Value(0, label="x2")
 
+# weights for inputs
+w1 = Value(-3.0, label="w1")
+w2 = Value(1.0, label="w2")
 
+# bias 
+b = Value(6.8813735870195432, label="b")
 
-visualize_graph(L, filename="viz3")
+x1w1 = x1 * w1; x1w1.label = "x1*w1"
+x2w2 = x2 * w2; x2w2.label = "x2*w2"
+
+x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = "x1*w1 + x2*w2"
+n = x1w1x2w2 + b; n.label="n"
+o = n.tanh(); o.label="o"
+
+o.backward()
+
+visualize_graph(o, filename="neuron")
